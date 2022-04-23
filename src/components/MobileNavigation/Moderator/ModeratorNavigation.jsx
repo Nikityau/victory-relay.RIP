4f0e7664b8322fd31a4b3fada3c5dd423386818c
CITG RSTU <CITG-14@CITG-14.rgups.edu>
2022-04-24 00:01:21 +0300

    fix admin mob ver

--- src/components/MobileNavigation/Moderator/ModeratorNavigation.jsx
+++ src/components/MobileNavigation/Moderator/ModeratorNavigation.jsx
@@ -1,62 +1,73 @@
 import React, {useContext, useEffect, useRef, useState} from 'react';
-import { Link, Redirect } from 'react-router-dom'
+import {Link, Redirect, useLocation} from 'react-router-dom'
 
 import classes from "./ModeratorNavigation.module.css";
 
 import home from '../../../assets/icons/mobile-icons/home.svg'
+import redHome from '../../../assets/icons/mobile-icons/red-home.svg'
 import exit from '../../../assets/icons/mobile-icons/enter.svg'
+import redEXit from '../../../assets/icons/mobile-icons/enter-red.svg'
 
 const ModeratorNavigation = ({isRun, moderator, token}) => {
 
-    const [left, setLeft] = useState(0)
+    const location = useLocation()
 
     const lineRef = useRef(null);
+
     const homeRef = useRef(null);
     const exitRef = useRef(null);
 
     useEffect(() => {
         Left()
-    }, [left])
+        //console.log(location.pathname)
+    }, [location])
 
     const Left = () => {
-        if(!lineRef.current) return;
+        const path = location.pathname;
 
-        if(left == 0) {
-            const ls_storage = localStorage.getItem('mob_line_admin');
-            ls_storage ? setLeft(ls_storage) : toHome()
+        if (!lineRef.current || !homeRef.current || !exitRef.current) return
+
+        if(path.includes('stage')) {
+            toHome()
+            return
         }
 
-        const coords = localStorage.getItem('mob_line_admin')
-        const offset = coords || left;
-
-        lineRef.current.style.left = offset + 'px';
+        if(path.includes('exit')) {
+            toExit()
+            return
+        }
     }
 
     const toHome = () => {
-        const offset = homeRef.current.offsetLeft + homeRef.current.clientWidth / 2 - lineRef.current.clientWidth / 2;
-        localStorage.setItem('mob_line_admin', offset)
-        setLeft(offset);
+        const offset = homeRef.current.offsetLeft + (homeRef.current.clientWidth / 2 || 11);
+        console.log(homeRef.current.offsetLeft, homeRef.current.clientWidth)
+        //console.log(offset)
+        setLeft(offset)
     }
     const toExit = () => {
-        const offset = exitRef.current.offsetLeft + exitRef.current.clientWidth / 2 - lineRef.current.clientWidth / 2;
-        localStorage.setItem('mob_line_admin', offset)
-        console.log(offset)
+        const offset = exitRef.current.offsetLeft + exitRef.current.clientWidth / 2;
         setLeft(offset)
+    }
+    const setLeft = (offsetLeft) => {
+        const halfLine = lineRef.current.clientWidth / 2;
+        lineRef.current.style.left = offsetLeft - halfLine + 'px';
     }
 
     return (
         <div className={classes.moderNavContainer}>
             <div className={classes.wrapper}>
-                <div ref={lineRef} className={classes.line}/>
-                <div onClick={toHome} ref={homeRef} className={[classes.home, isRun ? classes.touchPrevent : ''].join(' ')}>
+                <div ref={lineRef} id={'moder-line'} className={classes.line}/>
+                <div onClick={toHome} ref={homeRef}
+                     className={[classes.home, isRun ? classes.touchPrevent : ''].join(' ')}>
                     <Link to={`/admin/${token}/choose-stage`}>
-                        <img src={home} alt={'img'}/>
+                        <img src={location.pathname.includes('stage') ? redHome : home} alt={'img'}/>
                     </Link>
                 </div>
-                <div onClick={toExit} ref={exitRef} className={[classes.exit, isRun ? classes.touchPrevent : ''].join(' ')}>
-                   <Link to={`/admin/${token}/exit`}>
-                       <img src={exit} alt={'img'}/>
-                   </Link>
+                <div onClick={toExit} ref={exitRef}
+                     className={[classes.exit, isRun ? classes.touchPrevent : ''].join(' ')}>
+                    <Link to={`/admin/${token}/exit`}>
+                        <img src={location.pathname.includes('exit') ? redEXit : exit} alt={'img'}/>
+                    </Link>
                 </div>
             </div>
         </div>
